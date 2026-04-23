198876017d3873b39700e2002a8829555f54f779
Hyago Diaz <hyagodiasf@gmail.com>
2026-04-23 15:28:56 -0300

    UPGRADE

--- python_basico_aplicacoes/06_exercicio_paravra_secreta.py
+++ python_basico_aplicacoes/06_exercicio_paravra_secreta.py
@@ -1,40 +1,48 @@
-import os, random as ran, time as t
+import os
+import random
+import time
 
-aleatory_list_word = ['javascript','ruby', 'lisp', 'java', 'python','html']
-secret_word = ran.choice(aleatory_list_word)
+word_list  = ['javascript','ruby', 'lisp', 'java', 'python','html']
+secret_word = random.choice(word_list)
 
-aux_secret_word = list(secret_word.replace(secret_word[:], "*" * (len(secret_word))))
+##hidden_word  = list(secret_word.replace(secret_word[:], "*" * (len(secret_word))))
 
-print(f"Palavra secreta: " + "".join(aux_secret_word))
+hidden_word = ["*"] * len(secret_word)
+
+print(f"Palavra secreta: " + "".join(hidden_word ))
 
 count = 0
+
 print(f"""
 BEM-VINDO AO JOGO
 ACERTE A PALAVRA SECRETA
 """
 )
 
-while "*" in (aux_secret_word):
+while "*" in (hidden_word):
     try:
       
-        typed_word = input("Digite uma letra: ").lower()
+        typed_letter = input("Digite uma letra: ").lower()
 
-        if(len(typed_word) != 1 or typed_word.isdigit()):
+        if(not typed_letter.isalpha() or len(typed_letter) != 1):
             print("Você informou um número ou mais de uma letra.")
+            print("Informe apenas UMA letras válidas")
             continue
-        elif(typed_word in secret_word):
+        count +=1 
+
+        if(typed_letter in secret_word):
             for inx, char in enumerate(secret_word):
-                if(char == typed_word):
-                    aux_secret_word[inx] = typed_word
-            print(f"Acertou! A palavra é: " + "".join(aux_secret_word))
+                if(char == typed_letter):
+                    hidden_word [inx] = typed_letter
+            print(f"Acertou! A palavra é: " + "".join(hidden_word ))
         else:
             print(f'A letra "{typed_word}" não está na palavra secreta.')
    
-    except:
-        pass
-    count +=1 
+    except Exception as e:
+        print(f"Erro: {e}")
+    
 
 print("PARABÉNS! VOCÊ GANHOU: " + secret_word)
 print(f'Tentativas: {count}')
-t.sleep(5)
-os.system('cls')
+time.sleep(5)
+os.system('cls' if os.name == 'nt' else 'clear')
